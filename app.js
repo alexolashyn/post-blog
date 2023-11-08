658a8f11815aff468b976458ccfa2ef3f691d283
sashka.olashyn <sashka.olashyn@gmail.com>
2023-11-08 20:10:12 +0200

    fix commit

--- app.js
+++ app.js
@@ -11,28 +11,16 @@ app.use(express.static('./public'));
 app.set("view engine", "ejs");
 
 
-app.set('trust proxy', 1);
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit')
-app.use(helmet());
-app.use(cors());
-app.use(xss());
-app.use(rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100
-}));
-
 const authRouter = require('./routes/auth');
 const blogRouter = require('./routes/blog')
 
 app.use('/api/auth', authRouter);
 app.use('/api/blog', blogRouter);
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
     res.redirect('/api/auth/login');
-});
+
+})
 
 const port = process.env.PORT || 5000;
 
